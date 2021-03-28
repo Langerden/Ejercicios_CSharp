24e1efc38a45eb864b14d4238e5732f8c8400329
Denise Langer <Langer_D@hotmail.com>
2021-03-28 20:34:59 -0300

    Ejercicio 17

---
+++ Ejercicio_17/Ejercicio_17/Boligrafo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impresora
+{
+    public class Boligrafo
+    {
+        private const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(ConsoleColor color, short tinta)
+        {
+            this.color = color;
+            this.tinta = tinta;
+        }
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+
+        private void SetTinta (short tinta)
+        {
+            short valorActual = GetTinta();
+            if ((valorActual + tinta) >= 0 && (valorActual + tinta) <= cantidadTintaMaxima)
+                this.tinta = (short)(valorActual + tinta);
+            else if (valorActual + tinta < 0)
+                this.tinta = 0;
+            else
+                this.tinta = cantidadTintaMaxima;
+        }
+
+        public void Recargar()
+        {
+            SetTinta(cantidadTintaMaxima);
+        }
+
+        public bool Pintar (short gasto, out string dibujo)
+        {
+            dibujo = "";
+            if (GetTinta() > 0 && gasto < 0)
+            {
+                this.SetTinta(gasto);
+                return true;
+            }
+            else
+            { 
+                return false;
+            }
+        }
+        public string PrintGasto (int tintaActual)
+        {
+            string asterisco = "";
+
+            if (tintaActual <= 0)
+                asterisco = "No hay mas tinta";
+            else {
+                for (int i = 0; i < tintaActual; i++)
+                {
+                    if (i % 10 == 0)
+                        asterisco += "\n";
+                    asterisco += "*";
+                }
+            }
+            return asterisco;
+        }
+    }
+}
---
+++ Ejercicio_17/Ejercicio_17/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Impresora;
+
+namespace Ejercicio_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string printAzul;
+            string printRojo;
+
+            Boligrafo azul = new Boligrafo(ConsoleColor.Blue, 100);
+            Boligrafo rojo = new Boligrafo(ConsoleColor.Red, 50);
+
+            Console.WriteLine("Color: " + azul.GetColor());
+            Console.WriteLine("Tinta: " + azul.GetTinta());
+            
+            azul.Pintar(-20, out printAzul);
+            Console.WriteLine("Tinta Actual: " + azul.GetTinta() + "\nNivel tinta: \n" + azul.PrintGasto(azul.GetTinta()));
+
+            azul.Pintar(-40, out printAzul);
+            Console.WriteLine("Tinta Actual: " + azul.GetTinta() + "\nNivel tinta: \n" + azul.PrintGasto(azul.GetTinta()));
+
+            azul.Pintar(-60, out printAzul);
+            Console.WriteLine("Tinta Actual: " + azul.GetTinta() + "\nNivel tinta: \n" + azul.PrintGasto(azul.GetTinta()));
+
+            azul.Recargar();
+            Console.WriteLine("\nTinta: " + azul.GetTinta());
+
+            Console.ReadKey();
+
+        }
+    }
+}
