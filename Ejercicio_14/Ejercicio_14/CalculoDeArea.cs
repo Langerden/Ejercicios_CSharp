7a7dacf3caa3cd8f2a1ddeef0fad0fd0093cc3ff
Denise Langer <Langer_D@hotmail.com>
2021-03-28 20:34:35 -0300

    Ejercicio 14

---
+++ Ejercicio_14/Ejercicio_14/CalculoDeArea.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Realizar una clase llamada CalculoDeArea que posea 3 métodos de clase (estáticos) que realicen el cálculo del área que corresponda:
+//a. double CalcularCuadrado(double)
+//b. double CalcularTriangulo(double, double)
+//c. double CalcularCirculo(double)
+//El ingreso de los datos como la visualización se deberán realizar desde el método Main().
+namespace Ejercicio_14
+{
+    public class CalculoDeArea
+    {
+        public static double CalcularCuadrado(double lado)
+        {
+            if (lado > 0)
+                return lado * lado;
+            else
+                return -1;
+        }
+
+        public static double CalcularTriangulo(double lado, double altura)
+        {
+            if (lado > 0 && altura > 0)
+                return (lado * altura) / 2;
+            else
+                return -1;
+        }
+
+        public static double CalcularCirculo(double radio)
+        {
+            if (radio > 0)
+                return Math.Round(Math.PI * radio * radio,2);
+            else
+                return -1;
+        }
+    }
+}
---
+++ Ejercicio_14/Ejercicio_14/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Realizar una clase llamada CalculoDeArea que posea 3 métodos de clase (estáticos) que realicen el cálculo del área que corresponda:
+//a. double CalcularCuadrado(double)
+//b. double CalcularTriangulo(double, double)
+//c. double CalcularCirculo(double)
+//El ingreso de los datos como la visualización se deberán realizar desde el método Main().
+namespace Ejercicio_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cuadrado = 0;
+            double baseTriangulo = 0;
+            double alturaTriangulo = 0;
+            double radioCirculo = 0;
+
+            Console.WriteLine("Ingrese el valor de un lado del cuadrado para calcular su area: ");
+            cuadrado = Double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Ingrese el valor de la base del triangulo: ");
+            baseTriangulo = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Ahora ingrese el valor de la altura del triangulo para calcular su area: ");
+            alturaTriangulo = Double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Ingrese el valor del radio del circulo para calcular el area: ");
+            radioCirculo = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("El area del cuadrado es: " + CalculoDeArea.CalcularCuadrado(cuadrado));
+            Console.WriteLine("El area del triangulo es: " + CalculoDeArea.CalcularTriangulo(baseTriangulo, alturaTriangulo));
+            Console.WriteLine("El area del circulo es: " + CalculoDeArea.CalcularCirculo(radioCirculo));
+           
+            Console.ReadKey();
+        }
+    }
+}
