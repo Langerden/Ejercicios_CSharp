b0005c673e9ae2e74acd77fe537090cbc30adf56
Denise Langer <Langer_D@hotmail.com>
2021-03-28 20:33:31 -0300

    Ejercicio 5

---
+++ Ejercicio5/Biblioteca/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Cliente
+    {
+        private string nombre;
+        private string apellido;
+
+        public string GetNombreCompleto()
+        {   
+            //Inyectar un String
+            return $"{this.nombre}{this.apellido}";
+        }
+    }
+}
