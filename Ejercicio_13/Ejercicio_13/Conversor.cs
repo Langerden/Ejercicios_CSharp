db0ca25b65f97f89c3e2af0e1803fbe0110a8a4f
Denise Langer <Langer_D@hotmail.com>
2021-03-28 20:34:24 -0300

    Ejercicio 13

---
+++ Ejercicio_13/Ejercicio_13/Conversor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_13
+{
+    public class Conversor
+    {
+        public static string DecimalBinario(int number)
+        {
+            string numeroBinario = "";
+            char[] numeroBinarioCorrecto;
+
+            if (number < 0)
+                return "Numero no valido";
+            else
+            {
+                while (number > 0)
+                {
+                    numeroBinario+= number % 2;
+                    number /= 2;                   
+                }
+            }
+            numeroBinarioCorrecto = numeroBinario.ToCharArray();
+            Array.Reverse(numeroBinarioCorrecto);
+            return new string(numeroBinarioCorrecto);
+        }
+
+        public static int BinarioDecimal(string binario)
+        {
+            const int DIVISOR = 10;
+            long numeroBinario = long.Parse(binario);
+            int modulo = 0;
+            int numeroDecimal = 0;
+
+            for (long i = numeroBinario, j = 0; i > 0; i /= DIVISOR, j++){
+                modulo = (int) i % DIVISOR;
+                if (modulo != 1 && modulo != 0)
+                    return -1;
+                else
+                    numeroDecimal += modulo * (int) Math.Pow(2, j);
+            }
+            return numeroDecimal;
+        }
+    }
+}
---
+++ Ejercicio_13/Ejercicio_13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Desarrollar una clase llamada Conversor, que posea dos métodos de clase (estáticos): string DecimalBinario(int).
+//Convierte un número de entero a binario. int BinarioDecimal(string). Convierte un número binario a entero.
+//NOTA: no utilizar los atajos del lenguaje, hacerlo mediante estructuras repetitivas y de control.
+namespace Ejercicio_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDecimal = 0;
+            string numeroBinario = "";
+            
+            Console.WriteLine("Escriba un numero decimal");
+            numeroDecimal = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escriba un numero binario");
+            numeroBinario = Console.ReadLine();
+
+            Console.WriteLine("Numero decimal convertido a binario: " + Conversor.DecimalBinario(numeroDecimal));
+            Console.WriteLine("Numero binario convertido a decimal: " + Conversor.BinarioDecimal(numeroBinario).ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
