bf5a51ed2afafb12fdec33f56dbd470cfa116e65
Denise Langer <Langer_D@hotmail.com>
2021-03-28 20:34:51 -0300

    Ejercicio 16

---
+++ Ejercicio_16/Ejercicio_16/Alumno.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    public class Alumno
+    {
+        private byte nota1;
+        private byte nota2;
+        private float notaFinal;
+        private string apellido;
+        private int legajo;
+        private string nombre;
+
+        public Alumno(string apellido, int legajo, string nombre)
+        {
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nombre = nombre;
+        }
+
+        public void Estudiar (byte notaIngresada1, byte notaIngresada2)
+        {
+            this.nota1 = notaIngresada1;
+            this.nota2 = notaIngresada2;
+        }
+
+        public void CalcularFinal()
+        {
+            float notaFinal = -1;
+
+            if (this.nota1 >= 4 && this.nota2 >= 4)
+            {
+                Random nota = new Random();
+                notaFinal = nota.Next(5, 10);
+            }
+            this.notaFinal = notaFinal;       
+        }
+        public string Mostrar()
+        {
+            string datoAlumno = $"Nota 1: {this.nota1}, Nota 2: {this.nota2}, Apellido: {this.apellido}, Nombre:{this.nombre}, Legajo:{this.legajo}\t";
+            if (this.notaFinal != -1)
+                datoAlumno += $"Nota Final: {this.notaFinal}";
+            else
+                datoAlumno += "Alumno desaprobado";
+            return datoAlumno;
+        }
+    }
+}
---
+++ Ejercicio_16/Ejercicio_16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno facundo = new Alumno("Falcone", 105450, "Facundo");
+            Alumno denise = new Alumno("Langer", 110053, "Denise");
+            Alumno cata = new Alumno("Heredia", 140047, "Catalina");
+
+            facundo.Estudiar(8, 9);
+            denise.Estudiar(3, 8);
+            cata.Estudiar(10, 10);
+            
+            facundo.CalcularFinal();
+            denise.CalcularFinal();
+            cata.CalcularFinal();
+
+            Console.WriteLine(facundo.Mostrar());
+            Console.WriteLine(denise.Mostrar());
+            Console.WriteLine(cata.Mostrar());
+
+            Console.ReadKey();
+        }
+    }
+}
