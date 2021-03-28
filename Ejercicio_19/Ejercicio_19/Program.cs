80efe26698015979f06e69316bba3041c0d44eb3
Denise Langer <Langer_D@hotmail.com>
2021-03-28 20:35:13 -0300

    Ejercicio 19

---
+++ Ejercicio_19/Ejercicio_19/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador sumador1 = new Sumador();
+            Sumador sumador2 = new Sumador();
+
+            Console.WriteLine(sumador1.Sumar(10, 15));
+            Console.WriteLine(sumador1.Sumar(15, 15));
+            Console.WriteLine(sumador2.Sumar("Aguante ", "Bokita"));
+
+            int dato = (int)sumador1;
+            int dato2 = (int)sumador2;
+
+            Console.WriteLine(dato);
+            Console.WriteLine(dato2);
+            
+            Console.WriteLine(sumador1+sumador2);
+            Console.WriteLine(sumador1|sumador2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio_19/Ejercicio_19/Sumador.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_19
+{
+    public class Sumador
+    {
+        private int cantidadSumas;
+        public Sumador() : this(0) {}
+
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+
+        public long Sumar(long a, long b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+        public string Sumar(string a, string b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+
+        public static explicit operator int(Sumador sumador) {
+            return sumador.cantidadSumas;
+        }
+
+        public static bool operator | (Sumador sumador1, Sumador sumador2)
+        {   if (sumador1.cantidadSumas == sumador2.cantidadSumas)
+                return true;
+            else
+                return false;
+        }
+
+        public static long operator + (Sumador sumador1, Sumador sumador2)
+        {
+            return sumador1.cantidadSumas + sumador2.cantidadSumas;
+        }
+    }
+}
