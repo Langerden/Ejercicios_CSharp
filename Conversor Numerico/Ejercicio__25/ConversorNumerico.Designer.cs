5d85476169a39f99e455705c9f6608a791fe8c9c
Denise Langer <Langer_D@hotmail.com>
2021-04-03 17:32:41 -0300

    Ejercicio_25

---
+++ Conversor Numerico/Ejercicio__25/ConversorNumerico.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace Ejercicio__25
+{
+    partial class ConversorForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBinarioDecimal = new System.Windows.Forms.Label();
+            this.lblDecimalBinario = new System.Windows.Forms.Label();
+            this.txtBinario = new System.Windows.Forms.TextBox();
+            this.txtDecimal = new System.Windows.Forms.TextBox();
+            this.txtResultadoBin = new System.Windows.Forms.TextBox();
+            this.txtResultadoDec = new System.Windows.Forms.TextBox();
+            this.btnBinToDec = new System.Windows.Forms.Button();
+            this.btnDecToBin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBinarioDecimal
+            // 
+            this.lblBinarioDecimal.AutoSize = true;
+            this.lblBinarioDecimal.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBinarioDecimal.ForeColor = System.Drawing.Color.Gold;
+            this.lblBinarioDecimal.Location = new System.Drawing.Point(24, 35);
+            this.lblBinarioDecimal.Name = "lblBinarioDecimal";
+            this.lblBinarioDecimal.Size = new System.Drawing.Size(135, 21);
+            this.lblBinarioDecimal.TabIndex = 0;
+            this.lblBinarioDecimal.Text = "Binario a Decimal";
+            // 
+            // lblDecimalBinario
+            // 
+            this.lblDecimalBinario.AutoSize = true;
+            this.lblDecimalBinario.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDecimalBinario.Location = new System.Drawing.Point(24, 88);
+            this.lblDecimalBinario.Name = "lblDecimalBinario";
+            this.lblDecimalBinario.Size = new System.Drawing.Size(135, 21);
+            this.lblDecimalBinario.TabIndex = 1;
+            this.lblDecimalBinario.Text = "Decimal a Binario";
+            // 
+            // txtBinario
+            // 
+            this.txtBinario.Location = new System.Drawing.Point(175, 35);
+            this.txtBinario.Name = "txtBinario";
+            this.txtBinario.Size = new System.Drawing.Size(83, 20);
+            this.txtBinario.TabIndex = 2;
+            this.txtBinario.Leave += new System.EventHandler(this.txtBinario_Leave);
+            // 
+            // txtDecimal
+            // 
+            this.txtDecimal.Location = new System.Drawing.Point(175, 91);
+            this.txtDecimal.Name = "txtDecimal";
+            this.txtDecimal.Size = new System.Drawing.Size(83, 20);
+            this.txtDecimal.TabIndex = 3;
+            this.txtDecimal.Leave += new System.EventHandler(this.txtDecimal_Leave);
+            // 
+            // txtResultadoBin
+            // 
+            this.txtResultadoBin.Enabled = false;
+            this.txtResultadoBin.Location = new System.Drawing.Point(383, 91);
+            this.txtResultadoBin.Name = "txtResultadoBin";
+            this.txtResultadoBin.Size = new System.Drawing.Size(124, 20);
+            this.txtResultadoBin.TabIndex = 5;
+            // 
+            // txtResultadoDec
+            // 
+            this.txtResultadoDec.Enabled = false;
+            this.txtResultadoDec.Location = new System.Drawing.Point(383, 33);
+            this.txtResultadoDec.Name = "txtResultadoDec";
+            this.txtResultadoDec.Size = new System.Drawing.Size(124, 20);
+            this.txtResultadoDec.TabIndex = 4;
+            // 
+            // btnBinToDec
+            // 
+            this.btnBinToDec.BackColor = System.Drawing.Color.Orange;
+            this.btnBinToDec.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBinToDec.ForeColor = System.Drawing.Color.Black;
+            this.btnBinToDec.Location = new System.Drawing.Point(283, 33);
+            this.btnBinToDec.Name = "btnBinToDec";
+            this.btnBinToDec.Size = new System.Drawing.Size(75, 23);
+            this.btnBinToDec.TabIndex = 6;
+            this.btnBinToDec.Text = "=>";
+            this.btnBinToDec.UseVisualStyleBackColor = false;
+            this.btnBinToDec.Click += new System.EventHandler(this.btnBinToDec_Click);
+            // 
+            // btnDecToBin
+            // 
+            this.btnDecToBin.BackColor = System.Drawing.Color.Orange;
+            this.btnDecToBin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecToBin.ForeColor = System.Drawing.Color.Black;
+            this.btnDecToBin.Location = new System.Drawing.Point(283, 89);
+            this.btnDecToBin.Name = "btnDecToBin";
+            this.btnDecToBin.Size = new System.Drawing.Size(75, 23);
+            this.btnDecToBin.TabIndex = 7;
+            this.btnDecToBin.Text = "=>";
+            this.btnDecToBin.UseVisualStyleBackColor = false;
+            this.btnDecToBin.Click += new System.EventHandler(this.btnDecToBin_Click);
+            // 
+            // ConversorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(519, 160);
+            this.Controls.Add(this.btnDecToBin);
+            this.Controls.Add(this.btnBinToDec);
+            this.Controls.Add(this.txtResultadoBin);
+            this.Controls.Add(this.txtResultadoDec);
+            this.Controls.Add(this.txtDecimal);
+            this.Controls.Add(this.txtBinario);
+            this.Controls.Add(this.lblDecimalBinario);
+            this.Controls.Add(this.lblBinarioDecimal);
+            this.ForeColor = System.Drawing.Color.Gold;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ConversorForm";
+            this.ShowIcon = false;
+            this.Text = "Conversor Numerico";
+            this.Load += new System.EventHandler(this.ConversorForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBinarioDecimal;
+        private System.Windows.Forms.Label lblDecimalBinario;
+        private System.Windows.Forms.TextBox txtBinario;
+        private System.Windows.Forms.TextBox txtDecimal;
+        private System.Windows.Forms.TextBox txtResultadoBin;
+        private System.Windows.Forms.TextBox txtResultadoDec;
+        private System.Windows.Forms.Button btnBinToDec;
+        private System.Windows.Forms.Button btnDecToBin;
+    }
+}
+
---
+++ Conversor Numerico/Ejercicio__25/ConversorNumerico.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Conversores;
+
+namespace Ejercicio__25
+{
+    public partial class ConversorForm : Form
+    {
+        public ConversorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ConversorForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBinario_Leave(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(txtBinario.Text))
+            {
+                NumeroBinario numeroBinario = new NumeroBinario(txtBinario.Text);
+                txtBinario.Text = numeroBinario.GetNumeroBinario();
+            } else
+            {
+                MessageBox.Show("Numero no valido", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtBinario.Focus();
+            }
+        }
+
+        private void txtDecimal_Leave(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(txtDecimal.Text) && Double.TryParse(txtDecimal.Text, out double decimalValid))
+            {
+                NumeroDecimal numeroDecimal = new NumeroDecimal(decimalValid);
+                txtDecimal.Text = numeroDecimal.GetNumeroDecimal().ToString();
+            }
+            else
+            {
+                MessageBox.Show("Numero no valido", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtBinario.Focus();
+            }
+        }
+
+        private void btnBinToDec_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(txtBinario.Text) && (!String.IsNullOrEmpty(txtBinario.Text)))
+            {
+                NumeroBinario numeroBinario = new NumeroBinario(txtBinario.Text);
+                if (((NumeroDecimal)numeroBinario).GetNumeroDecimal() == -1)
+                {
+                    txtResultadoDec.Text = "Error de convercion";
+                }
+                else
+                {
+                    txtResultadoDec.Text = ((NumeroDecimal)numeroBinario).GetNumeroDecimal().ToString();
+                }
+            }
+        }
+
+        private void btnDecToBin_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(txtDecimal.Text) && Double.TryParse(txtDecimal.Text, out double decimalValid))
+            {
+                NumeroDecimal numeroDecimal = new NumeroDecimal(decimalValid);
+                txtResultadoBin.Text = ((NumeroBinario)numeroDecimal).GetNumeroBinario();
+            }
+        }
+    }
+}
