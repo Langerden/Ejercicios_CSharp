c5e27539a42e8a695c5a56e3c3e100f78c8c42e4
Denise Langer <Langer_D@hotmail.com>
2021-04-10 17:41:00 -0300

    Ejercicio_33

---
+++ Ejercicio_33/Ejercicio_33/Libro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_33
+{
+    public class Libro
+    {
+        private List<string> paginas;
+
+        public Libro()
+        {
+            this.paginas = new List<string>();
+        }
+
+        public string this[int i]
+        {
+            get
+            {
+                if (i < this.paginas.Count && i >= 0)
+                    return this.paginas[i];
+                else
+                    return "VACIO";
+            }
+            set
+            {
+                if (i < this.paginas.Count && i >= 0)
+                    this.paginas[i] = value;
+                else
+                    this.paginas.Add(value);
+            }
+        }
+    }
+
+}
---
+++ Ejercicio_33/Ejercicio_33/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro = new Libro();
+
+            libro[0] = "Epilogo";
+            libro[1] = "Capitulo 1";
+            libro[2] = "Capitulo 2";
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(libro[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
