e04df9b11b4a9bed00c517a2af29bddce37234fe
Denise Langer <Langer_D@hotmail.com>
2021-04-10 17:38:43 -0300

    Ejercicio_32

---
+++ Ejercicio_32/Ejercicio_32/Jugador.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_32
+{
+    public class Jugador
+    {
+        private int dni;
+        private string nombre;
+        private int partidosJugados;
+        private int totalGoles;
+
+        private Jugador()
+        {
+            this.partidosJugados = 0;
+            this.partidosJugados = 0;
+            this.totalGoles = 0;
+        }
+        public Jugador(int dni, string nombre) : this()
+        {
+            this.dni = dni;
+            this.nombre = nombre;            
+        }
+
+        public Jugador(int dni, string nombre, int partidosJugados, int totalGoles) : this(dni, nombre)
+        {
+            this.partidosJugados = partidosJugados;
+            this.totalGoles = totalGoles;
+        }
+
+        public int Dni
+        {
+            get { return this.dni; }
+            set { this.dni = value; }
+        }
+
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+
+        public int PartidosJugados
+        {
+            get { return this.partidosJugados; }
+        }
+
+        public float PromedioGoles
+        {
+            get
+            {
+                if (this.partidosJugados != 0)
+                {
+                   return (float)this.totalGoles / this.partidosJugados;
+                }
+                else
+                    return 0;
+            }
+        }
+
+        public int TotalGoles
+        {
+            get { return this.totalGoles; }
+        }
+
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            return sb.Append($"DNI: {this.Dni.ToString()} NOMBRE: {this.Nombre.ToString()} PARTIDOS JUGADOS: {this.PartidosJugados.ToString()} TOTAL GOLES: {this.TotalGoles.ToString()}").ToString();
+        }
+
+        public static bool operator == (Jugador j1, Jugador j2)
+        {
+            return j1.Dni == j2.Dni;
+        }
+
+        public static bool operator !=(Jugador j1, Jugador j2)
+        {
+            return !(j1.Dni == j2.Dni);
+        }
+    }
+}
---
+++ Ejercicio_32/Ejercicio_32/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jugador j1 = new Jugador(11, "Carlos", 3, 5);
+            Jugador j2 = new Jugador(22, "Pedro", 7, 1);
+            Jugador j3 = new Jugador(33, "Leo", 10, 15);
+            Equipo original = new Equipo(3, "YKC");
+
+            Console.WriteLine("Promedio Goles J1: " + j1.PromedioGoles);
+            Console.WriteLine("Promedio Goles J2: " + j2.PromedioGoles);
+            Console.WriteLine("Promedio Goles J3: " + j3.PromedioGoles);
+
+            if (original + j1 && original + j2 && original + j3)
+            {
+                Console.WriteLine(j1.MostrarDatos());
+                Console.WriteLine(j2.MostrarDatos());
+                Console.WriteLine(j3.MostrarDatos());
+            }
+            Console.ReadKey();
+        }
+    }
+}
