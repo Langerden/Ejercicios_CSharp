a47fcf6f306b32c3e06acb028e5381c33dacbf5b
Langerden <langer_d@hotmail.com>
2021-07-03 02:53:08 -0300

    Ejercicio 37 - Threads + Eventos

---
+++ Ejercicio_67/Ejercicio_67/Temporizador.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejercicio_67
+{
+    public delegate void EncargadoTiempo();
+    public sealed class Temporizador
+    {
+        private Thread hilo;
+        private int intervalo;
+        public event EncargadoTiempo eventoTiempo;
+
+        public bool Activo
+        {
+            get { return (!(this.hilo is null)) && this.hilo.IsAlive; }
+            set
+            {
+                if (value)
+                {
+                    if (this.hilo is null)
+                    {
+                        this.hilo = new Thread(this.Corriendo);
+                        this.hilo.Start();
+                    }
+                }
+                else
+                {
+                    if (this.hilo != null && this.hilo.IsAlive)
+                        this.hilo.Abort();
+                }
+            }
+        }
+
+        public int Intervalo { get => intervalo; set => intervalo = value; }
+
+        public void Corriendo()
+        {
+            while (true)
+            {
+                Thread.Sleep(Intervalo);
+                if (this.eventoTiempo != null)
+                    this.eventoTiempo.Invoke();
+            }
+
+        }
+
+    }
+
+}
