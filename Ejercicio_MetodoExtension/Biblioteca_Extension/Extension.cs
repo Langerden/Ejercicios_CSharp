52025a572986e84f4c3fb5e31846ec2c2e4d7f2f
Langerden <langer_d@hotmail.com>
2021-07-03 03:09:23 -0300

    Metodos de Extension - Estaciones

---
+++ Ejercicio_MetodoExtension/Biblioteca_Extension/Extension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Extension
+{
+    public static class Extension
+    {
+        public static string ObtenerPlacaCronica(this DateTime fecha, Estaciones estaciones)
+        {
+            DateTime fechaEstacion = new DateTime(); 
+            int diferencia = 0;
+
+            switch (estaciones)
+            {
+                case Estaciones.Invierno:
+                    fechaEstacion = new DateTime(DateTime.Now.Year, 6,21);
+                    break;
+                case Estaciones.Primavera:
+                    fechaEstacion = new DateTime(DateTime.Now.Year, 9, 21);
+                    break;               
+                case Estaciones.Verano:
+                    fechaEstacion = new DateTime(DateTime.Now.Year, 12, 21);
+                    break;                
+                case Estaciones.Otonio:
+                    fechaEstacion = new DateTime(DateTime.Now.Year, 3, 21);
+                    break;
+            }
+
+            if (fechaEstacion < fecha)
+            {
+                fechaEstacion = fechaEstacion.AddYears(1);
+            }
+
+            diferencia = (int)(fechaEstacion - fecha).TotalDays;
+
+            //return $"Faltan {diferencia} dias para el {estaciones}";
+            return $"Faltan {diferencia} dias para el {Enum.GetName(typeof(Estaciones), estaciones)}";
+        }
+    }
+}
---
+++ Ejercicio_MetodoExtension/Ejercicio_MetodoExtension/Program.cs
@@ -0,0 +1,31 @@
+﻿using Biblioteca_Extension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ejercicio_MetodoExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            string invierno = date.ObtenerPlacaCronica(Estaciones.Invierno);
+            string verano = date.ObtenerPlacaCronica(Estaciones.Verano);
+            string otonio = date.ObtenerPlacaCronica(Estaciones.Otonio);
+            string primavera = date.ObtenerPlacaCronica(Estaciones.Primavera);
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine(invierno);
+            Console.WriteLine(verano);
+            Console.WriteLine(otonio);
+            Console.WriteLine(primavera);
+            Console.ReadKey();
+        }
+    }
+}
