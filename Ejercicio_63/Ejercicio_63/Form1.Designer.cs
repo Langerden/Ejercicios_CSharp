c9cbe6b9553da15541d245c645cc350f30dabf7f
Langerden <langer_d@hotmail.com>
2021-07-03 02:50:44 -0300

    Ejercicio 63 (Thread)

---
+++ Ejercicio_63/Ejercicio_63/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Ejercicio_63
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHora = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Location = new System.Drawing.Point(24, 25);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(16, 13);
+            this.lblHora.TabIndex = 0;
+            this.lblHora.Text = "...";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(27, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(745, 359);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.lblHora);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        //private System.Windows.Forms.Label lblHora2;
+    }
+}
+
---
+++ Ejercicio_63/Ejercicio_63/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using Ejercicio_67;
+
+namespace Ejercicio_63
+{
+    public delegate void Delegado(object objeto);
+    public delegate void DelegadoVoid();
+    public partial class Form1 : Form
+    {
+        Temporizador temp = new Temporizador();
+        //Thread hiloPrincipal;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //public void AsignarHora(Object objeto)
+        //{
+        //    if (this.lblHora.InvokeRequired)
+        //    {
+        //        Delegado miDelegado = new Delegado(AsignarHora);
+        //        Object[] arrayObject = new Object[] { objeto};
+        //        this.Invoke(miDelegado, arrayObject);
+        //    } else { this.lblHora.Text = objeto.ToString(); }
+        //}
+
+        public void AsignarHora2()
+        {
+            if (this.lblHora.InvokeRequired)
+            {
+                DelegadoVoid delegado2 = new DelegadoVoid(this.AsignarHora2);
+                this.Invoke(delegado2);
+            } else
+            {
+                this.lblHora.Text = DateTime.Now.ToString();
+            }
+        }
+
+        //Realizar la actualización de la hora 1 vez por segundo utilizando alguna estructura de control dada en clase.
+        //private void ActualizarHora()
+        //{
+        //    while (true)
+        //    {
+        //        this.AsignarHora(DateTime.Now);
+        //    }
+        //}
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            temp.Activo = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            temp.Activo = true;
+            temp.Intervalo = 2000;
+            temp.eventoTiempo += this.AsignarHora2;
+        }
+    }
+}
